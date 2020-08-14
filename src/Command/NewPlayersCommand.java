12b71085dede91a62d254ad3dfe3b130fa6811b2
Guilherme Deschamps <guilhermerd6@hotmail.com>
2020-08-14 02:29:34 -0300

    Criado novo objeto Player e iniciado o comando de adicionar os players

---
+++ src/Command/NewPlayersCommand.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Command;
+
+import Control.GameControl;
+
+/**
+ *
+ * @author guilh
+ */
+public class NewPlayersCommand extends Command {
+
+    String player1Name;
+    String player2Name;
+
+    public NewPlayersCommand(GameControl gameControl, String player1Name, String player2Name) {
+        super(gameControl);
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    @Override
+    void execute() {
+        gameControl.setPlayers(player1Name, player2Name);
+    }
+
+}
