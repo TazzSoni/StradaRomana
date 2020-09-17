b1c0f9a2f1e40cdedc543c17bcba30c9b6cc5e58
Guilherme Deschamps <guilhermerd6@hotmail.com>
2020-09-17 20:46:13 -0300

    Criadas primeiras classes necessárias para o state dos WagonTiles

---
+++ src/State/Movement.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package State;
+
+/**
+ *
+ * @author guilh
+ */
+public class Movement {
+    
+    protected MovementState movementState;
+    
+    public Movement(){}
+    
+    public void commonMove(){
+        
+    }
+    
+    public void sidewaysMove(){
+        
+    }
+    
+    public void diagonalMove(){
+        
+    }
+    
+    public void extraMove(){
+        
+    }
+    
+    public void staking(){
+        
+    }
+    
+}
